565aee0c617f8d7b2a42fbeccc8252ca7c782bcd
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-04-14 14:31:33 +0300

    Change apiTOdatabase to add info for time and place to the database

--- apiTOdatabase.php
+++ apiTOdatabase.php
@@ -14,11 +14,10 @@
 	if (!$db_selected) {
 	  die ('Can\'t use db : ' . mysql_error());
 	}
+
 	$jsondata = file_get_contents('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson');
     	
-	$page = $_SERVER['http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'];
-	$sec = "50";
-	header("Refresh: $sec; url=$page");
+	
 	
     //convert json object to php associative array
     $data = json_decode($jsondata, true);
@@ -26,9 +25,12 @@
     foreach ($data['features'] as &$feature) {
         $longitude = $feature['geometry']['coordinates'][0];
         $latitude = $feature['geometry']['coordinates'][1];
+	$time = $feature['properties']['time'];
+	$place = $feature['properties']['place'];
+	
        
-		$query ="INSERT INTO `Coordinates` (`longitude`, `latitude`)
-			SELECT * FROM (SELECT $longitude,$latitude) AS tmp
+		$query ="INSERT INTO `Coordinates` (`longitude`, `latitude`,`time`,`place`)
+			SELECT * FROM (SELECT $longitude,$latitude,$time,$place) AS tmp
 			WHERE NOT EXISTS (
     			SELECT * FROM `Coordinates` WHERE longitude = $longitude AND latitude = $latitude
 			) LIMIT 1;";
