a1496a94666ee50ccfbc5054ee5a23c8c77f0f74
Slavi <s.zmiycharov@gmail.com>
2016-05-09 15:13:42 +0300

    Upload file ekatte.php

---
+++ ekatte.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+function myFunction() {
+	var input = document.getElementById('mySearch').value;
+	console.log(input);
+	
+}
+</script>
+
+<p>Enter:</p>
+<input type="search" id="mySearch" placeholder="Search for town..">
+
+<button onclick="myFunction()">Search</button>
+
+
+
+</body>
+</html>
