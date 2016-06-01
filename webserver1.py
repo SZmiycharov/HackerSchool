f4ad79fafc655a4addfec5af2d9f8778eb4cea00
Slavi <s.zmiycharov@gmail.com>
2016-06-01 10:32:33 +0300

    update file webserver.py

--- webserver1.py
+++ webserver1.py
@@ -1,14 +1,14 @@
 import socket
 import re
 
-# Standard socket stuff:
 host = '' 
 port = 8080
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #change socket behaviour - to be able to reconnect faster
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((host, port))
-sock.listen(1) # don't queue up any requests
+#don't queue requests - listen for only 1
+sock.listen(1) 
 
 # Loop forever, listening for requests:
 while True:
@@ -16,8 +16,8 @@ while True:
     print "Connection from: " + `caddr`
     req = csock.recv(1024) # get the request, 1kB max
     print req
-    # Look in the first line of the request for a move command
-    # A move command should be e.g. 'http://server/move?a=90'
+    # address should be sth like GET /move?a=20&b=3 HTTP/1.1
+
     match = re.match('GET .*?a=(\d+)', req)
     if match:
         a = match.group(1)
@@ -28,20 +28,21 @@ while True:
         print "b: " + b
 	sumOfBoth = int(a) + int(b)
 	print "a + b = %d" % (sumOfBoth)
-        csock.sendall("""HTTP/1.0 200 OK
+	print "\n"
+        csock.sendall("""HTTP/1.1 200 OK
 Content-Type: text/html
 <html>
 <head>
 <title>Success</title>
 </head>
 <body>
-Boo!
+YEY!
 </body>
 </html>
 """)
     else:
         # If there was no recognised command then return a 404 (page not found)
         print "Returning 404"
-        csock.sendall("HTTP/1.0 404 Not Found\r\n")
+        csock.sendall("HTTP/1.1 404 Not Found\r\n")
 
     csock.close()
