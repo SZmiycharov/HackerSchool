c7d44524fac72f7aaa344977bba0f73f32a823be
Slavi <s.zmiycharov@gmail.com>
2016-06-10 09:42:33 +0300

    upload strawberries visualisation - html to visualise output from program

--- strawberries python.py
+++ strawberries python.py
@@ -1,55 +1,167 @@
-def MakeNeighborsBadStrawberries(arr, arri, arrj, day, indexi, indexj):
-    if(indexi-1 != -1 and arr[indexi-1][indexj] != 1 and
-       arr[indexi-1][indexj] != day-1):
-        arr[indexi - 1][indexj] = day
-    if(indexj-1 != -1 and arr[indexi][indexj-1] != 1 and
-       arr[indexi][indexj - 1] != day-1):
-        arr[indexi][indexj - 1] = day
-    if(indexj + 1 != arrj and arr[indexi][indexj + 1] != 1 and
-       arr[indexi][indexj + 1] != day-1):
-        arr[indexi][indexj + 1] = day
-    if(indexi+1 != arri and arr[indexi+1][indexj] != 1 and
-       arr[indexi+1][indexj] != day-1):
-        arr[indexi + 1][indexj] = day
+def GoUp(arr, K, L, currentDay, maxDay, indexi, indexj):
+        indexi = indexi + 1
+        while indexi < K:
+                if currentDay >= maxDay:
+                        return
+                if currentDay <= arr[indexi][indexj]:
+                        arr[indexi][indexj] = currentDay
+                        indexi = indexi + 1
+                        currentDay = currentDay + 1
+                else: break
+
+def GoDown(arr, K, L, currentDay, maxDay, indexi, indexj):
+        indexi = indexi - 1
+        while indexi >= 0:
+                if currentDay >= maxDay:
+                        return
+                if currentDay <= arr[indexi][indexj]:
+                        arr[indexi][indexj] = currentDay
+                        indexi = indexi - 1
+                        currentDay = currentDay + 1
+                else: break
+
+def GoLeft(arr, K, L, currentDay, maxDay, indexi, indexj):
+        indexj = indexj - 1
+        while indexj >= 0:
+                if currentDay >= maxDay:
+                        return
+                if currentDay <= arr[indexi][indexj]:
+                        arr[indexi][indexj] = currentDay
+                        indexj = indexj - 1
+                        currentDay = currentDay + 1
+                else: break
+
+def GoRight(arr, K, L, currentDay, maxDay, indexi, indexj):
+        indexj = indexj + 1
+        while indexj < L:
+                if currentDay >= maxDay:
+                        return
+                if currentDay <= arr[indexi][indexj]:
+                        arr[indexi][indexj] = currentDay
+                        indexj = indexj + 1
+                        currentDay = currentDay + 1
+                else: break
+
+count = 0
+
+while True:
+  try:
+     K = int(input("Enter K: "))       
+  except StandardError:
+     print("Not an integer!")
+     continue
+  else:
+     if(K<0):
+         print("Input must be >=0 !")
+         continue
+     else:
+         break
+while True:
+  try:
+     L = int(input("Enter L: "))       
+  except StandardError:
+     print("Not an integer!")
+     continue
+  else:
+     if(L<0):
+         print("Input must be >=0 !")
+         continue
+     else:
+         break
+while True:
+  try:
+     R = int(input("Enter R: "))       
+  except StandardError:
+     print("Not an integer!")
+     continue
+  else:
+     if(R<0):
+         print("Input must be >=0 !")
+         continue
+     else:
+         break
+while True:
+  try:
+     i1 = int(input("Enter i1: "))       
+  except StandardError:
+     print("Not an integer!")
+     continue
+  else:
+     if(i1<0):
+         print("Input must be >=0 !")
+         continue
+     else:
+         break
+while True:
+  try:
+     j1 = int(input("Enter j1: "))       
+  except StandardError:
+     print("Not an integer!")
+     continue
+  else:
+     if(j1<0):
+         print("Input must be >=0 !")
+         continue
+     else:
+         break
         
-K = 8
-L = 10
+choice = raw_input("Another point? y or n: ")
+if choice == 'y':
+        while True:
+          try:
+             i2 = int(input("Enter i2: "))       
+          except StandardError:
+             print("Not an integer!")
+             continue
+          else:
+             if(i2<0):
+                 print("Input must be >=0 !")
+                 continue
+             else:
+                 break
+        while True:
+          try:
+             j2 = int(input("Enter j2: "))       
+          except StandardError:
+             print("Not an integer!")
+             continue
+          else:
+             if(j2<0):
+                 print("Input must be >=0 !")
+                 continue
+             else:
+                 break
 
-i1 = 1
-j1 = 6
-i2 = 3
-j2 = 7
+R = R + 2
 
+arr = [[10000 for x in range(K)] for y in range(L)] 
 
-arr = [[0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0],
-       [0, 0 ,0 ,0 ,0 ,0 ,1 ,0 ,0, 0],
-       [0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0],
-       [0, 0 ,0 ,0 ,0 ,0 ,0 ,1 ,0, 0],
-       [0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0],
-       [0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0],
-       [0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0],
-       [0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0]]
-
-day = 2
-MakeNeighborsBadStrawberries(arr, K, L, day, i1, j1)
-MakeNeighborsBadStrawberries(arr, K, L, day, i2, j2)
-day = day + 1
-MakeNeighborsBadStrawberries(arr, K, L, day, i1 - 1, j1)
-MakeNeighborsBadStrawberries(arr, K, L, day, i1, j1 - 1)
-MakeNeighborsBadStrawberries(arr, K, L, day, i1, j1 + 1)
-MakeNeighborsBadStrawberries(arr, K, L, day, i1 + 1, j1)
-MakeNeighborsBadStrawberries(arr, K, L, day, i2 - 1, j2)
-MakeNeighborsBadStrawberries(arr, K, L, day, i2, j2 - 1)
-MakeNeighborsBadStrawberries(arr, K, L, day, i2, j2 + 1)
-MakeNeighborsBadStrawberries(arr, K, L, day, i2 + 1, j2)
-
-for p in range(8):
-    for q in range(10):
-        print arr[p][q],
-    print "\n"
+arr[i1][j1] = 1
+if i2 != 0 or j2 != 0:
+        arr[i2][j2] = 1
         
+currentDay = 1
+while True:
+        for i in range(K):
+                for j in range (L):
+                        if arr[i][j] == currentDay:
+                                GoUp(arr, K, L, currentDay + 1, R, i, j)
+                                GoDown(arr, K, L, currentDay + 1, R, i, j)
+                                GoLeft(arr, K, L, currentDay + 1, R, i, j)
+                                GoRight(arr, K, L, currentDay + 1, R, i, j)
+        currentDay = currentDay + 1
+        if currentDay >= R: break
 
 
+for i in range (K):
+        for j in range (L):
+                print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in arr]))
+                if arr[i][j] == 10000:
+                        count = count + 1
+        print("\n")
+
+print(count)
 
 
 
