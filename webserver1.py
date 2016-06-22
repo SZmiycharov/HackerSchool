f1f075b936468c9a21c8deb72a8fda2a39ba24f8
Slavi <s.zmiycharov@gmail.com>
2016-06-22 11:04:56 +0300

    update webserver

--- webserver1.py
+++ webserver1.py
@@ -52,17 +52,22 @@ Server: SLAVI
 Content-Type: text/plain\n
 """)
 	
+
 	while True:
 		fileData = f.read()
 		if fileData == '': break
 		sock.sendall(fileData)
+	
 	f.close()
 	sock.close()
+
+
+
 
 host = '' 
 port = ''
 folder = ''
-
+#get port and folder from cmd line
 try:
 	opts, args = getopt.getopt(sys.argv[1:],"hp:f:",["port=", "folder="])
 except getopt.GetoptError:
@@ -77,7 +82,6 @@ for opt, arg in opts:
 	elif opt in ("-f", "--folder"):
         	folder = arg
 port = int(port)
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #change socket behaviour - to be able to reconnect faster
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -86,12 +90,14 @@ try:
 except socket.error as e:
 	print(str(e))
 sock.listen(5) 
-print("                   ************SERVER STARTED************")
 
-def threaded_client(csock):
+def threaded_client(conn):
+	print("in threaded_client")
 	while True:
-	    req = csock.recv(4096) # get the request, 3kB max
+	    req = conn.recv(4096) # get the request, 3kB max
 	    print req
+	    # req should be sth like GET /move?a=20&b=3 HTTP/1.1
+
 	    match = re.match('GET .*?.=(\d+)', req)
 	    if match:
 		a = match.group(1)
@@ -105,7 +111,7 @@ def threaded_client(csock):
 			print "a + b = %d" % (sumOfBoth)
 			print "\n"
 
-			csock.send("""HTTP/1.1 200 OK
+			conn.send("""HTTP/1.1 200 OK
 		Server: SLAVI
 		Content-Type: text/html
 
@@ -115,34 +121,24 @@ def threaded_client(csock):
 		</body>
 		</html>
 				""" % (sumOfBoth))
-			csock.send("\n")	
+			conn.send("\n")
+	    
 	    elif re.match('GET /(.*) ', req):
 		match = re.match('GET /(.*) ', req)
 		fileName = match.group(1)
-		#get the file
-		t = threading.Thread(target=RetrFile, args=(csock, fileName))
+		t = threading.Thread(target=RetrFile, args=(conn, fileName))
 		t.start()
 	    else:
-		csock.send("Invalid request!\n")
-	csock.close()
+		conn.send("Invalid request!\n")
+	conn.close()
 
+
+print("                   ************SERVER STARTED************")
 while True:
-	csock, caddr = sock.accept()
-	print "Connection from: " + `caddr`
-	tr = threading.Thread(target=threaded_client, args=(csock,))
+	conn, addr = sock.accept()
+	print "Connection from: " + `addr`
+	tr = threading.Thread(target=threaded_client, args=(conn,))
 	tr.start()
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
