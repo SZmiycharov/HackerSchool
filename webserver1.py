f83ab6896abb103021081f7eb02e2a086e60fe48
Slavi <s.zmiycharov@gmail.com>
2016-06-22 12:14:40 +0300

    update webserver MULTICLIENTS WORK NOWgit add .!

--- webserver1.py
+++ webserver1.py
@@ -1,31 +1,17 @@
 import socket
+import re
 import threading
+import sys, getopt
+import os
 
-class ThreadedServer(object):
-    def __init__(self, host, port, folder):
-        self.host = host
-        self.port = port
-	self.folder = folder
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((self.host, self.port))
-
-    def listen(self):
-        self.sock.listen(5)
-        while True:
-            client, address = self.sock.accept()
-            client.settimeout(60)
-            threading.Thread(target = self.listenToClient,args = (client,address)).start()
-
-    def RetrFile:
+def RetrFile(conn, filename):
 	try:		
-		filePath = self.folder + "/" + filename
+		filePath = folder + "/" + filename
         	f = open(filePath, 'rb')
 		print(filePath)
 	except IOError:
 		errorMsg = "File could not be found!"
-		sock.send(errorMsg)
-		sock.close()
+		conn.send(errorMsg)
 		return
 
 	match = re.match('.*\.(.*)', filename)
@@ -33,92 +19,111 @@ class ThreadedServer(object):
 
 	
 	if(fileType == 'py' or fileType == 'txt'):
-		sock.send("""HTTP/1.1 200 OK
+		conn.send("""HTTP/1.1 200 OK
 Server: SLAVI
 Content-Type: text/plain\n
 """)
 	elif(fileType == "html" or fileType == "php"):
-		sock.send("""HTTP/1.1 200 OK
+		conn.send("""HTTP/1.1 200 OK
 Server: SLAVI
 Content-Type: text/html\n
 """)
 	elif(fileType == 'png'):
-		sock.send("""HTTP/1.1 200 OK
+		conn.send("""HTTP/1.1 200 OK
 Server: SLAVI
 Content-Type: image/png\n
 """)
 	elif(fileType == 'jpg'):
-		sock.send("""HTTP/1.1 200 OK
+		conn.send("""HTTP/1.1 200 OK
 Server: SLAVI
 Content-Type: image/jpeg\n
 """)
-	
-
 	while True:
 		fileData = f.read()
 		if fileData == '': break
-		sock.sendall(fileData)
-	f.close()   
+		conn.sendall(fileData)
+	print("finished while loop")
+	f.close()
+	conn.close()
 
-    def listenToClient(self, client, address):
-        while True:
-            try:
-                req = client.recv(1024)
-                if data:
-                    match = re.match('GET .*?.=(\d+)', req)
-		    if match:
-			a = match.group(1)
-			print "a: " + a
+host = '' 
+port = ''
+folder = ''
+#get port and folder from cmd line
+try:
+	opts, args = getopt.getopt(sys.argv[1:],"hp:f:",["port=", "folder="])
+except getopt.GetoptError:
+	print 'webserver1.py -p <port> -f <folder>'
+	sys.exit(2)
+for opt, arg in opts:
+	if opt == '-h':
+        	print 'webserver1.py -p <port> -f <folder>'
+        	sys.exit()
+    	elif opt in ("-p", "--port"):
+        	port = arg
+	elif opt in ("-f", "--folder"):
+        	folder = arg
+port = int(port)
 
-		    match = re.match('GET .*&.=(\d+)', req)
-		    if match:
+
+def threaded_client(conn):
+	while True:
+	    req = conn.recv(4096) # get the request, 3kB max
+	    print req
+	    # req should be sth like GET /move?a=20&b=3 HTTP/1.1
+		
+	    match = re.match('GET .*?.=(\d+)', req)
+	    if match:
+		a = match.group(1)
+		print "a: " + a
+
+	    	match = re.match('GET .*&.=(\d+)', req)
+	    	if match:
 			b = match.group(1)
 			print "b: " + b
 			sumOfBoth = int(a) + int(b)
 			print "a + b = %d" % (sumOfBoth)
 			print "\n"
 
-			csock.send("""HTTP/1.1 200 OK
+			conn.send("""HTTP/1.1 200 OK
 		Server: SLAVI
 		Content-Type: text/html
+
 		<html>
 		<body>
 		<p><b> sum: %d </b></p>
 		</body>
 		</html>
 				""" % (sumOfBoth))
-			csock.close()
+			conn.send("\n")
+	    
+	    elif re.match('GET /(.*) ', req):
+		match = re.match('GET /(.*) ', req)
+		fileName = match.group(1)
+		RetrFile(conn, fileName)
+	    else:
+		conn.send("Invalid request!\n")
+	return
 
-		    
-		    else:
-			match = re.match('GET /(.*) ', req)
-			fileName = match.group(1)
-			#get the file
-			t = threading.Thread(target=RetrFile, args=("RetrThread", csock, fileName))
-			t.start()
-                else:
-                    raise error('Client disconnected')
-            except:
-                client.close()
-                return False
 
-if __name__ == "__main__":
-	host = '' 
-	port = ''
-	folder = ''
-	#get port and folder from cmd line
+print("                   ************SERVER STARTED************")
+while True:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	#change socket behaviour - to be able to reconnect faster
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	try:
-		opts, args = getopt.getopt(sys.argv[1:],"hp:f:",["port=", "folder="])
-	except getopt.GetoptError:
-		print 'webserver1.py -p <port> -f <folder>'
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			print 'webserver1.py -p <port> -f <folder>'
-			sys.exit()
-	    	elif opt in ("-p", "--port"):
-			port = arg
-		elif opt in ("-f", "--folder"):
-			folder = arg
-	port = int(port)
-	ThreadedServer('', port, folder).listen()
+		sock.bind((host, port))
+	except socket.error as e:
+		print(str(e))
+	sock.listen(5) 
+	conn, addr = sock.accept()
+	print "Connection from: " + `addr`
+	tr = threading.Thread(target=threaded_client, args=(conn,))
+	tr.start()
+
+
+
+
+
+
+
