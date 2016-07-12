99d3e74a8636189545f2d34d697f207fd5ecbafe
Slavi <s.zmiycharov@gmail.com>
2016-07-12 16:02:05 +0300

    upload graph - for okryjnosti module

---
+++ graph.py
@@ -0,0 +1,30 @@
+""" A Python Class
+A simple Python graph class, demonstrating the essential 
+facts and functionalities of graphs.
+"""
+
+
+class Graph(object):
+
+    def __init__(self, graph_dict={}):
+        """ initializes a graph object """
+        self.__graph_dict = graph_dict
+
+    def find_path(self, start_vertex, end_vertex, path=None):
+        """ find a path from start_vertex to end_vertex 
+            in graph """
+        if path == None:
+            path = []
+        graph = self.__graph_dict
+        path.append(start_vertex)
+        if start_vertex == end_vertex:
+            return path
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_path = self.find_path(vertex, 
+                                               end_vertex, 
+                                               path)
+                if extended_path: 
+                    return extended_path
+        return None
+
