9539d6f467f763876eb910b3176306fd8bfcbc4e
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-02 11:21:14 +0300

    update

--- ekatte/xlsToPostgres.py
+++ ekatte/xlsToPostgres.py
@@ -8,7 +8,7 @@ cur = conn.cursor()
 
 data_xls = pd.read_excel('/home/slavi/Desktop/ekatte/important/Ek_obl.xls', index_col=None)
 data_xls.to_csv('/home/slavi/Desktop/Ek_obl.csv', encoding='utf-8')
-cur.execute("COPY oblast FROM '/home/slavi/Desktop/Ek_obl.csv' DELIMITER ',' CSV")
+cur.execute("COPY helperOblast FROM '/home/slavi/Desktop/Ek_obl.csv' DELIMITER ',' CSV")
 conn.commit()
 
 
@@ -35,16 +35,39 @@ conn.commit()
 cur.execute("DELETE FROM helperObstina WHERE abc = 'abc'")
 conn.commit()
 
+cur.execute('ALTER TABLE helperOblast DROP COLUMN "id"')
+conn.commit()
+cur.execute("DELETE FROM helperOblast WHERE abc = 'abc'")
+conn.commit()
+
 cur.execute('ALTER TABLE public.helperobstina ADD COLUMN oblast text')
 conn.commit()
 
-cur.execute('INSERT INTO helperObstina(oblast) SELECT oblast FROM helperSelishte')
+cur.execute('''UPDATE helperobstina
+SET oblast=subquery.oblast
+FROM (SELECT oblast, obstina
+      FROM helperselishte ) AS subquery
+WHERE helperobstina.obstina=subquery.obstina''')
 conn.commit()
 
-cur.execute('INSERT INTO obstina SELECT * FROM helperObstina')
+data_xls = pd.read_excel('/home/slavi/Desktop/ekatte/important/Ek_obl.xls', index_col=None)
+data_xls.to_csv('/home/slavi/Desktop/Ek_obl.csv', encoding='utf-8')
+cur.execute("COPY oblast FROM '/home/slavi/Desktop/Ek_obl.csv' DELIMITER ',' CSV")
+conn.commit()
+cur.execute('ALTER TABLE oblast DROP COLUMN "id"')
+conn.commit()
+cur.execute("DELETE FROM oblast WHERE abc = 'abc'")
 conn.commit()
 
-cur.execute('INSERT INTO selishte SELECT * FROM helperSelishte')
+cur.execute('''INSERT INTO obstina(obstina, ekatte, name, category, document, abc, oblast) 
+	SELECT obstina, ekatte, name, category, document, abc, oblast FROM helperobstina''')
+conn.commit()
+
+cur.execute('''INSERT INTO selishte(ekatte, t_v_m, name, oblast, obstina, kmetstvo, kind, category) 
+	SELECT ekatte, t_v_m, name, oblast, obstina, kmetstvo, kind, category FROM helperselishte''')
+
+
+
 
 
 
