f3fa6ac384dedca73e1ac41a09bd5cb8c2055864
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-02 11:51:27 +0300

    helper.php works correctly

--- ekatte/helper.php
+++ ekatte/helper.php
@@ -5,38 +5,29 @@ $passwd = '3111';
 $db = 'ekatte';
 $port = 5432;
 $host = '';
-
-$strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd";
+$strCnx = "port=$port dbname=$db user=$user password=$passwd";
 $cn = pg_connect($strCnx);
 
 $entry = $_GET['entry'];
 
-
-$query = "SELECT ek_atte.t_v_m, ek_atte.name, ek_obl.name as oblast, ek_obst.name as obstina
-	FROM ek_atte
-	INNER JOIN ek_obst ON ek_atte.obstina = ek_obst.obstina
-	INNER JOIN ek_obl ON ek_atte.oblast = ek_obl.oblast
-	WHERE ek_atte.name LIKE '{$entry}%'";
+$query = "SELECT selishte.t_v_m, selishte.name, oblast.name as oblast, obstina.name as obstina
+  FROM selishte
+  INNER JOIN obstina ON selishte.obstina = obstina.obstina
+  INNER JOIN oblast ON selishte.oblast = oblast.oblast
+  WHERE selishte.name LIKE '{$entry}%'";
 $result = pg_query($query);
-
 header("Content-type: text/xml");
 
-// Start XML file, echo parent node
 echo "<Places>"; 
-
-// Iterate through the rows, printing XML nodes for each
 while ($row = @pg_fetch_assoc($result)){
-  // ADD TO XML DOCUMENT NODE
   echo '<place ';
-
   echo 'type="' . $row['t_v_m'] . '" ';
   echo 'name="' . $row['name'] . '" ';
   echo 'oblast="' . $row['oblast'] . '" ';
   echo 'obstina="' . $row['obstina'] . '" ';
-  
   echo '/>';
 }
-
-// End XML file
 echo '</Places>';
+
 ?>
+
