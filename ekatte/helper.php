ae32db7a224aeb2483e298f277869774f4899a7c
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-02 10:48:41 +0300

    two helper tables to insert afterwards

---
+++ ekatte/helper.php
@@ -0,0 +1,42 @@
+<?php
+
+$user = 'postgres';
+$passwd = '3111';
+$db = 'ekatte';
+$port = 5432;
+$host = '';
+
+$strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd";
+$cn = pg_connect($strCnx);
+
+$entry = $_GET['entry'];
+
+
+$query = "SELECT ek_atte.t_v_m, ek_atte.name, ek_obl.name as oblast, ek_obst.name as obstina
+	FROM ek_atte
+	INNER JOIN ek_obst ON ek_atte.obstina = ek_obst.obstina
+	INNER JOIN ek_obl ON ek_atte.oblast = ek_obl.oblast
+	WHERE ek_atte.name LIKE '{$entry}%'";
+$result = pg_query($query);
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo "<Places>"; 
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @pg_fetch_assoc($result)){
+  // ADD TO XML DOCUMENT NODE
+  echo '<place ';
+
+  echo 'type="' . $row['t_v_m'] . '" ';
+  echo 'name="' . $row['name'] . '" ';
+  echo 'oblast="' . $row['oblast'] . '" ';
+  echo 'obstina="' . $row['obstina'] . '" ';
+  
+  echo '/>';
+}
+
+// End XML file
+echo '</Places>';
+?>
--- ekatte/xlsToPostgres.py
+++ ekatte/xlsToPostgres.py
@@ -5,38 +5,46 @@ conn = psycopg2.connect("host='' port='5432' dbname='ekatte' user='postgres' pas
 cur = conn.cursor()
 
 
+
 data_xls = pd.read_excel('/home/slavi/Desktop/ekatte/important/Ek_obl.xls', index_col=None)
 data_xls.to_csv('/home/slavi/Desktop/Ek_obl.csv', encoding='utf-8')
 cur.execute("COPY oblast FROM '/home/slavi/Desktop/Ek_obl.csv' DELIMITER ',' CSV")
 conn.commit()
 
 
+data_xls = pd.read_excel('/home/slavi/Desktop/ekatte/important/Ek_obst.xls', index_col=None)
+data_xls.to_csv('/home/slavi/Desktop/Ek_obst.csv', encoding='utf-8')
+cur.execute("COPY helperObstina FROM '/home/slavi/Desktop/Ek_obst.csv' DELIMITER ',' CSV")
+conn.commit()
+
 
 data_xls = pd.read_excel('/home/slavi/Desktop/ekatte/important/Ek_atte.xls', index_col=None)
 data_xls.to_csv('/home/slavi/Desktop/Ek_atte.csv', encoding='utf-8')
-cur.execute("COPY grad FROM '/home/slavi/Desktop/Ek_atte.csv' DELIMITER ',' CSV")
+cur.execute("COPY helperSelishte FROM '/home/slavi/Desktop/Ek_atte.csv' DELIMITER ',' CSV")
 conn.commit()
 
 
-
-data_xls = pd.read_excel('/home/slavi/Desktop/ekatte/important/Ek_obst.xls', index_col=None)
-data_xls.to_csv('/home/slavi/Desktop/Ek_obst.csv', encoding='utf-8')
-cur.execute("COPY obstina FROM '/home/slavi/Desktop/Ek_obst.csv' DELIMITER ',' CSV")
+cur.execute('ALTER TABLE helperSelishte DROP COLUMN "id"')
+conn.commit()
+cur.execute("DELETE FROM helperSelishte WHERE abc = 'abc'")
 conn.commit()
 
-cur.execute('ALTER TABLE obstina DROP COLUMN "id"')
+
+cur.execute('ALTER TABLE helperObstina DROP COLUMN "id"')
 conn.commit()
-cur.execute("DELETE FROM obstina WHERE abc = 'abc'")
+cur.execute("DELETE FROM helperObstina WHERE abc = 'abc'")
 conn.commit()
 
-cur.execute('ALTER TABLE grad DROP COLUMN "id"')
-conn.commit()
-cur.execute("DELETE FROM grad WHERE abc = 'abc'")
+cur.execute('ALTER TABLE public.helperobstina ADD COLUMN oblast text')
 conn.commit()
 
-cur.execute('ALTER TABLE oblast DROP COLUMN "id"')
+cur.execute('INSERT INTO helperObstina(oblast) SELECT oblast FROM helperSelishte')
 conn.commit()
-cur.execute("DELETE FROM oblast WHERE abc = 'abc'")
+
+cur.execute('INSERT INTO obstina SELECT * FROM helperObstina')
 conn.commit()
+
+cur.execute('INSERT INTO selishte SELECT * FROM helperSelishte')
+
 
 
