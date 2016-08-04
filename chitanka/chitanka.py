90c726ed21bd8ffdf9bd1171548510e9b8243a66
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-04 11:59:34 +0300

    update

--- chitanka/chitanka.py
+++ chitanka/chitanka.py
@@ -4,25 +4,39 @@
 import os
 import re
 import time
+import fileinput
 
-
-words = raw_input()
-
-
-delimiters = "...", ",", " ", ".", "!", "?", "-"
+i = 0
+delimiters = "...", ",", " ", ".", "!", "?", "-", "_", "—","  "
 regexPattern = '|'.join(map(re.escape, delimiters))
+start = time.clock()
+
+for line in fileinput.input():
+  try:
+      f = open('/home/slavi/Desktop/HackerSchool/chitanka/words.txt', 'a')
+      with f:
+        for word in re.split(regexPattern, line):
+          if not word.isspace():
+            print word.lower()
+            i += 1
+            f.write(word + "\n")  
+  except IOError:
+      print "Could not open file:"
+      sys.exit()
+
+timeTaken = time.clock() - start
+print "Time taken<in seconds>: {}".format(timeTaken)
+print "Words: {}".format(i)
 
 
 
-try:
-    f = open('/home/slavi/Desktop/HackerSchool/chitanka/words.txt', 'w')
-except IOError:
-    print "Could not open file:"
-    sys.exit()
 
-with f:
-  for word in re.split(regexPattern, words):
-    f.write(word + "\n")
+
+
+
+
+
+
     
 
 
