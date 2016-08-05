0ff58acd251fe9facf7d3a38a262ba3f92f82a78
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-05 14:51:02 +0300

    THIS IS with 2nd input

--- fast tasks/Speed.py
+++ fast tasks/Speed.py
@@ -3,7 +3,7 @@ import networkx as nx
 minForAnswer = 10000000
 maxForAnswer = -10000000
 
-weights = [1, 7, 6, 12, 3, 9]
+weights = [3,5,8,16,8,19,72,9,6,5,28,15,19,16,13,1,6]
 
 G = nx.Graph()
 G.add_edge(1,2,weight=3)
@@ -16,8 +16,6 @@ G.add_edge(5,6,weight=72)
 G.add_edge(7,8,weight=9)
 G.add_edge(1,9,weight=6)
 G.add_edge(4,7,weight=5)
-
-
 G.add_edge(3,8,weight=28)
 G.add_edge(4,2,weight=15)
 G.add_edge(3,6,weight=19)
@@ -34,14 +32,14 @@ difference = 1000000
 currentMax = -100000000
 currentMin = 100000000
 
-nodes = [1,2,3,4,5,6,7]
+nodes = [1,2,3,4,5,6,7, 8, 9, 10]
 nodeshelper = nodes[:]
 iteration = 0
+
 
 for x in weights:
     for y in weights:
         print "iteration: {}".format(iteration)
-        print "({} {})".format(x,y)
         if x<y:
             minweight = x
             maxweight = y
@@ -62,29 +60,23 @@ for x in weights:
                 pass
         print "nodeshelper: {}".format(nodeshelper)
 
-        try:
-            if nx.edge_connectivity(H) and len(nodeshelper) == 0:
-                print "CONNECTED WITH ALL NODES!!!"
-                print len(H.edges())
-                for slavi in H.edges():
-                    try:
-                        weight = G[slavi[0]][slavi[1]]['weight']
-                    except:
-                        print "weight does not exist!"
-                        pass
-                    print "weight: {}".format(weight)
-                    if weight > currentMax:
-                        currentMax = weight
-                    if weight < currentMin:
-                        currentMin = weight
-                if difference > currentMax - currentMin:
-                    difference = currentMax - currentMin
-                    print "NEW DIFFERENCE: {} ; currentMax: {} ; currentMin: {}".format(difference, currentMax, currentMin)
-                    maxForAnswer = currentMax
-                    minForAnswer = currentMin      
-        except:
-            print "in except!"
-            pass
+
+        if len(nodeshelper) == 0 and nx.edge_connectivity(H):
+            print "CONNECTED WITH ALL NODES!!!"
+            print len(H.edges())
+            for slavi in H.edges():
+                weight = G[slavi[0]][slavi[1]]['weight']
+                print "weight: {}".format(weight)
+                if weight > currentMax:
+                    currentMax = weight
+                if weight < currentMin:
+                    currentMin = weight
+            if difference > currentMax - currentMin:
+                difference = currentMax - currentMin
+                print "NEW DIFFERENCE: {} ; currentMax: {} ; currentMin: {}".format(difference, currentMax, currentMin)
+                maxForAnswer = currentMax
+                minForAnswer = currentMin      
+
         currentMin = 1000000
         currentMax = -1000000
         nodeshelper = nodes[:]
