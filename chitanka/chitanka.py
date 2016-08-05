4a2a54ddf6c7a5e71dee43374d32999758cf8377
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-05 15:25:45 +0300

    speed optimization; check for empty input

--- chitanka/chitanka.py
+++ chitanka/chitanka.py
@@ -5,28 +5,39 @@ import os
 import re
 import time
 import fileinput
+import sys
+import select
+
+if select.select([sys.stdin,],[],[],0.0)[0]:
+    pass
+else:
+    print "Feed me books or otherwise I do nothing ;( !!"
+    sys.exit()
 
 i = 0
 delimiters = "...", ",", " ", ".", "!", "?", "-", "_", "—","  ", "    ", '""', "(", ")", '„', '“', '«', '»', ' ', '[', ']'
 regexPattern = '|'.join(map(re.escape, delimiters))
 start = time.clock()
 
-print fileinput.input().value
+
+try:
+      f = open('/home/slavi/Desktop/words.txt', 'a')
+except IOError:
+      print "Could not open file!"
+      sys.exit()
 
 for line in fileinput.input():
-  try:
-      f = open('/home/slavi/Desktop/words.txt', 'a')
-      with f:
         for word in re.split(regexPattern, line):
           if not word.isspace():
             i += 1
             f.write(word + "\n")
             if i%500000 == 0:
               print "{} words done!".format(i)
-  except IOError:
-      print "Could not open file:"
-      sys.exit()
 f.close()
+
+
+fileinput.close()
+
 
 timeTaken = time.clock() - start
 print "Time taken<in seconds>: {}".format(timeTaken)
