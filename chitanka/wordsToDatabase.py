30009aecc17bb6c5444e33a3fa27975afcbd6cd9
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-08 15:57:29 +0300

    check if word is in bulgarian

--- chitanka/wordsToDatabase.py
+++ chitanka/wordsToDatabase.py
@@ -4,6 +4,7 @@
 import psycopg2
 import time
 import sys
+import langid
 
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
@@ -20,19 +21,19 @@ try:
 			iterator += 1
 			if iterator%100000 == 0:
 				timeTakenFor100000words = time.time() - startInner
-
 				print "{} words iterated!".format(iterator)
 				print "Time taken for 100k words: {} seconds".format(timeTakenFor100000words)
-
 				startInner = time.time()
 			try:
 				if not line.isspace() and not line.isdigit():
+					
 					word = line.split('\n')[0].decode('utf-8').lower()
-					cur.execute("INSERT INTO dictionary VALUES ('%s');"% (word))
-					conn.commit()
+					if langid.classify(word)[0] == 'ru' or langid.classify(word)[0] == 'bg': 
+						query = "INSERT INTO dictionary VALUES ('%s');"% (word)
+						cur.execute(query)
+						conn.commit()
 			except Exception as e:
 				conn.rollback()
-				print e
 				pass
 		timeTaken = time.time() - startMain
 		print "Total time taken: {} seconds".format(timeTaken)
