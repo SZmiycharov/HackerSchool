1c1f7c112b25ba5888cff2e4ef8390e61c593ae8
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-15 14:01:32 +0300

    fail with this version

--- website/store/views.py
+++ website/store/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from .models import Category
+from .models import Category, Product
 
 
 class IndexView(generic.ListView):
@@ -13,3 +13,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Category
     template_name = 'store/detail.html'
+
+
+class ProductsView(generic.ListView):
+    template_name = 'store/products.html'
+    context_object_name = 'all_products'
+
+    def get_queryset(self):
+        return Product.objects.all()
+
