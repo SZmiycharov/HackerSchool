6ceebe810af4113d2cad194dfcf8f151245144e5
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-15 15:03:11 +0300

    Categories and products pages done!

--- website/store/urls.py
+++ website/store/urls.py
@@ -6,6 +6,6 @@ app_name = 'store'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^products/$', views.ProductsView.as_view(), name='products'),
 ]
