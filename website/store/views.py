3d539c78ea5de5f6de5d48012de3a91c23fc64e9
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-15 10:09:42 +0300

    background photo

--- website/store/views.py
+++ website/store/views.py
@@ -1,6 +1,5 @@
 from .models import Category
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -9,8 +8,5 @@ def index(request):
 
 
 def detail(request, category_id):
-    try:
-        category = Category.objects.get(pk=category_id)
-    except Category.DoesNotExist:
-        raise Http404("Category does not exist")
+    category = get_object_or_404(Category, pk=category_id)
     return render(request, 'store/detail.html', {'category': category,})
