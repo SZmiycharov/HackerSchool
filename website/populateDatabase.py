ba48f052bbb1a7d79450f3b521b4ba81ebabbb52
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-16 11:13:46 +0300

    search function admin panel

--- website/populateDatabase.py
+++ website/populateDatabase.py
@@ -3,12 +3,21 @@ from store.models import Category, Product
 fields = ['maker', 'model', 'description', 'price', 'category', 'product_logo', 'is_in_shopCart']
 pics = ['cpu_amd.jpeg', 'cpu_intel.jpeg', 'GPU.jpeg', 'index.jpeg', 'keyboard.jpg'] 
 
-for i in range(5):
+
+
+for i in range(5000):
 	for j in pics:
-		currmaker = 'maker' + str(i) + str(j)
-		currmodel = 'model' + str(i) + str(j)
-		currdescr = 'descr' + str(i) + str(j)
-		currprice = i + j
-		currcategory = i
-		currlogo = j
-		row = [currmaker, currmodel, currdescr, currprice, currcategory, currlogo, True]
+		a = Category()
+		a.name = 'category' + str(i)
+		a.category_logo = j
+		a.save()
+
+		b = Product()
+		b.maker = 'maker' + str(i)
+		b.model = 'model' + str(i)
+		b.description = 'descr' + str(i)
+		b.price = i
+		b.category = a
+		b.product_logo = j
+		b.is_in_shopCart = True
+		b.save()
--- website/store/admin.py
+++ website/store/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Category, Product
+from store.models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category_logo')
+    search_fields = ['name']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('maker', 'model', 'description', 'price', 'category', 'product_logo', 'is_in_shopCart')
+    search_fields = ['maker', 'model']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+
