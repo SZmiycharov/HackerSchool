255975ca693de345c706fab15e90abb9868295c6
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-16 13:45:14 +0300

    search function for maker works fine

--- website/store/urls.py
+++ website/store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^products/$', views.ProductsView.as_view(), name='products'),
     url(r'^shoppingCart/$', views.ShoppingCartView.as_view(), name='shoppingCart'),
+    url(r'^searchdetails/$', views.SearchDetailsView.as_view(), name='searchdetails'),
 ]
--- website/store/views.py
+++ website/store/views.py
@@ -1,7 +1,6 @@
 from django.views import generic
 from .models import Category, Product
 
-
 class IndexView(generic.ListView):
     template_name = 'store/index.html'
     context_object_name = 'all_categories'
@@ -29,5 +28,14 @@ class ShoppingCartView(generic.ListView):
 
     def get_queryset(self):
         return Product.objects.all().filter(is_in_shopCart=True)
+
+
+class SearchDetailsView(generic.ListView):
+    template_name = 'store/searchdetails.html'
+    context_object_name = 'searchresults'
+    querystring = ''
+
+    def get_queryset(self):
+        return Product.objects.all().filter(maker__icontains = self.request.GET.urlencode().split('=')[1])
 
 
