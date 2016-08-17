7428b84ed77853d4d4705f02e8b1044a15053ea5
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-17 10:55:39 +0300

    reg form adds users

--- website/store/views.py
+++ website/store/views.py
@@ -1,5 +1,9 @@
 from django.views import generic
 from .models import Category, Product
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 searchedfor = ''
 
@@ -47,6 +51,84 @@ class SearchDetailsView(generic.ListView):
             searchedfor = self.request.GET.urlencode().split('q=')[1]
 
         return Product.objects.all().filter(maker__icontains=searchedfor)
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'store/registration_form.html'
+
+    #display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    #process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            #cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #returns User objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('store:index')
+
+        return render(request, self.template_name, {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
