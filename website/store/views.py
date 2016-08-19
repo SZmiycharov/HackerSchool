1c30f6d000c936558de7ced4735ac40c8c3243d3
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-19 13:12:41 +0300

    login/logout/registration - should add sign up in login form

--- website/store/views.py
+++ website/store/views.py
@@ -1,23 +1,26 @@
 from django.views import generic
 from .models import Category, Product
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
+from django.contrib.auth import authenticate, login, REDIRECT_FIELD_NAME, login, logout
 from django.views.generic import View, FormView, RedirectView
-from .forms import UserForm
+from .forms import RegisterForm, LoginForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.utils.http import is_safe_url
-
+import pdb
+import sys
 
 searchedfor = ''
+
 
 class IndexView(generic.ListView):
     template_name = 'store/index.html'
     context_object_name = 'all_categories'
     paginate_by = 10
+
 
     def get_queryset(self):
         return Category.objects.all()
@@ -61,22 +64,21 @@ class SearchDetailsView(generic.ListView):
 
 
 class RegisterView(View):
-    form_class = UserForm
+    form_class = RegisterForm
     template_name = 'store/register.html'
 
-    #display blank form
     def get(self, request):
+        print >> sys.stderr, "\nGET FUNCTION REGISTER\n"
         form = self.form_class(None)
         return render(request, self.template_name, {'form': form})
 
-    #process form data
     def post(self, request):
+        print >> sys.stderr, "\nPOST FUNCTION REGISTER\n"
         form = self.form_class(request.POST)
 
         if form.is_valid():
             user = form.save(commit=False)
 
-            #cleaned (normalized) data
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user.set_password(password)
@@ -94,45 +96,44 @@ class RegisterView(View):
         return render(request, self.template_name, {'form': form})
 
 
-class LoginView(FormView):
-    success_url = '/index/'
-    template_name = 'store/login.html'
+class LoginView(View):
     form_class = AuthenticationForm
-    redirect_field_name = REDIRECT_FIELD_NAME
+    template_name = 'store/register.html'
 
-    @method_decorator(sensitive_post_parameters('password'))
-    @method_decorator(csrf_protect)
-    @method_decorator(never_cache)
-    def dispatch(self, request, *args, **kwargs):
-        # Sets a test cookie to make sure the user has cookies enabled
-        request.session.set_test_cookie()
+    # display blank form
+    def get(self, request):
+        print >> sys.stderr, "\nGET FUNCTION LOGIN\n"
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
 
-        return super(LoginView, self).dispatch(request, *args, **kwargs)
+    # process form data
+    def post(self, request):
+        print >> sys.stderr, "\nPOST FUNCTION LOGIN\n"
+        form = self.form_class(data=request.POST)
 
-    def form_valid(self, form):
-        auth_login(self.request, form.get_user())
+        print >> sys.stderr, form.is_valid()
+        if form.is_valid():
+            print >> sys.stderr, "\nform is valid\n"
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            print >> sys.stderr, password
 
-        # If the test cookie worked, go ahead and
-        # delete it since its no longer needed
-        if self.request.session.test_cookie_worked():
-            self.request.session.delete_test_cookie()
+            user = authenticate(username=username, password=password)
 
-        return super(LoginView, self).form_valid(form)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('store:index')
 
-    def get_success_url(self):
-        redirect_to = self.request.REQUEST.get(self.redirect_field_name)
-        if not is_safe_url(url=redirect_to, host=self.request.get_host()):
-            redirect_to = self.success_url
-        return redirect_to
+        return render(request, self.template_name, {'form': form})
 
 
-class LogoutView(RedirectView):
-    success_url = '/index/'
+class LogoutView(View):
     template_name = 'store/logout.html'
 
-    def get(self, request, *args, **kwargs):
-        auth_logout(request)
-        return super(LogoutView, self).get(request, *args, **kwargs)
+    def get(self, request):
+        logout(request)
+        return redirect('store:index')
 
 
 
