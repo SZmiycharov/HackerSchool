da79baf781a191aab2631686d76ad420d6a7f1d6
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-08-22 13:47:01 +0300

    add logging

--- website/website/settings.py
+++ website/website/settings.py
@@ -138,3 +138,22 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/store/'
 LOGOUT_REDIRECT_URL = '/store/logout'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/home/slavi/Desktop/PCStore.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
