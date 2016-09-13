11c4d6f4ea6d2be0b7510fbaec07611c5a491368
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-09-13 16:06:24 +0300

    fix file for postgres database

--- website/databaseToXml.php
+++ website/databaseToXml.php
@@ -1,5 +1,4 @@
 <?php
-
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header("Access-Control-Allow-Origin: *");
@@ -18,24 +17,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 $q=$_GET["q"];
 
-$db = new SQLite3('/home/slavi/Desktop/HackerSchool/website/db.sqlite3');
-$stmt = $db->prepare("SELECT DISTINCT maker FROM (
-            SELECT maker, 1 as ordering FROM store_product WHERE maker LIKE ?
+$user = 'postgres';
+$passwd = '3111';
+$db = 'pcstore';
+$port = 5432;
+$host = '';
+$strCnx = "port=$port dbname=$db user=$user password=$passwd";
+$cn = pg_connect($strCnx);
+
+$query = "SELECT DISTINCT maker, ordering FROM (
+            SELECT maker, 1 as ordering FROM store_product WHERE LOWER(maker) LIKE LOWER('{$q}%') 
             UNION 
-            SELECT maker, 2 as ordering FROM store_product WHERE maker LIKE ?)
-            ORDER BY ordering
-            LIMIT 5");
+            SELECT maker, 2 as ordering FROM store_product WHERE LOWER(maker) LIKE LOWER('%{$q}%')) as a 
+          ORDER BY ordering 
+          LIMIT 5";
 
-$stmt->bindValue(1, "{$q}%");
-$stmt->bindValue(2, "%{$q}%");
-
-
-$results = $stmt->execute();
+$result = pg_query($query);
 
 $hint = '';
 
-
-while ($row = $results->fetchArray()) {
+while ($row = @pg_fetch_assoc($result)){
   if ($hint=="")
   {
       $hint="<a href='" .
@@ -59,4 +60,5 @@ if ($hint=="") {
 }
 
 echo $response;
+
 ?> 
