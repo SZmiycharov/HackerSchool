d68322563a8730e2188f0d2b70aa308ac557ee84
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-04 13:59:10 +0300

    fix timing elapsed time

--- fast tasks/serializingModulesTest.pl
+++ fast tasks/serializingModulesTest.pl
@@ -3,7 +3,7 @@ use strict;
 use lib '/home/slavi/Desktop/HackerSchool/fast\ tasks';
 use serializeHash;
 use Try::Tiny;
-
+use Time::HiRes qw/ time /;
 
 $ARGV[0] or die "You should provide a file path from cmd line!";
 my $nested_file = "/home/slavi/Desktop/testfile.txt";
@@ -20,49 +20,52 @@ $beginning_hash{"b"}{Literature}   = $fh;
 $beginning_hash{"b"}{Mathematics}  = \@arr_to_nest;
 $beginning_hash{"b"}{Art}          = $blessed_var;
 
-print "************FreezeThawTest************\n";
-my $start_time = [Time::HiRes::gettimeofday()];
+print "\n*****************************FreezeThawTest*****************************\n";
+my $start_time = time;
 try{
 	testFreezeThaw(\%beginning_hash);
 } catch{
 	print "caught error: $_";
 };
-my $diff = Time::HiRes::tv_interval($start_time);
-print "Elapsed time: $diff ms\n";
-print "\n\n";
+my $end_time = time;
+my $elapsed_time = $end_time - $start_time;
+print "Elapsed time: ".$elapsed_time." ms\n\n";
 
-print "************JSONTest************\n";
-$start_time = [Time::HiRes::gettimeofday()];
+
+print "\n*****************************JSONTest*****************************\n";
+$start_time = time;
 try{
 	testJSON(\%beginning_hash);
 } catch{
 	print "caught error: $_";
 };
-$diff = Time::HiRes::tv_interval($start_time);
-print "Elapsed time: $diff ms\n";
-print "\n\n";
+$end_time = time;
+$elapsed_time = $end_time - $start_time;
+print "Elapsed time: ".$elapsed_time." ms\n\n";
 
-print "************YAMLTest************\n";
-$start_time = [Time::HiRes::gettimeofday()];
+
+print "\n*****************************YAMLTest*****************************\n";
+$start_time = time;
 try{
 	testYAML(\%beginning_hash);
 } catch{
 	print "caught error: $_";
 };
-$diff = Time::HiRes::tv_interval($start_time);
-print "Elapsed time: $diff ms\n";
-print "\n\n";
+$end_time = time;
+$elapsed_time = $end_time - $start_time;
+print "Elapsed time: ".$elapsed_time." ms\n\n";
 
-print "************BSONTest************\n";
-$start_time = [Time::HiRes::gettimeofday()];
+
+print "\n*****************************BSONTest*****************************\n";
+$start_time = time;
 try{
 	testBSON(\%beginning_hash);
 } catch{
 	print "caught error: $_";
 };
-$diff = Time::HiRes::tv_interval($start_time);
-print "Elapsed time: $diff ms\n";
-print "\n\n";
+$end_time = time;
+$elapsed_time = $end_time - $start_time;
+print "Elapsed time: ".$elapsed_time." ms\n\n";
 
 close $fh;
 
