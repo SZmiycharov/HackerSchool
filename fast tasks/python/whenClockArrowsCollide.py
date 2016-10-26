d692d3d9e1718eece6d9f763c257a06978287609
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-26 11:05:06 +0300

    fix the program - algorithm fine now

--- fast tasks/python/whenClockArrowsCollide.py
+++ fast tasks/python/whenClockArrowsCollide.py
@@ -1,38 +1,42 @@
 import datetime
+import math
+
 now = datetime.datetime.now()
 
 j = 0
 
-timesWhenArrowsCollide = []
 goingToCollide = False
 
-for i in range(0, 25):
-	if j == 60:
-		j = 0
-	if j/10 == 0:
-		if i/10 == 0:
-			if i == now.hour and now.minute <= j:
-				print "Time when arrows point same in current hour: 0{}:0{}".format(i, j)
-				goingToCollide = True
-				break
-		else:
-			if i == now.hour and now.minute <= j:
-				print "Time when arrows point same in current hour: {}:0{}".format(i, j)
-				goingToCollide = True
-				break
-	else:
-		if i/10 == 0:
-			if i == now.hour and now.minute <= j:
-				print "Time when arrows point same in current hour: 0{}:{}".format(i, j)
-				goingToCollide = True
-				break
-		else:
-			if i == now.hour and now.minute <= j:
-				print "Time when arrows point same in current hour: {}:{}".format(i, j)
-				goingToCollide = True
-				break
-	j += 5 
+for i in range(0, 24):
+	n = 720 * i / 11
+	n = round(n*100) / 100;
+	hour = math.floor(n/60)
 
+	minutes = math.floor(n%60)
+	
+	if hour < 25:
+		if minutes/10 == 0:
+			if hour/10 == 0:
+				if hour == now.hour and now.minute <= minutes:
+					print "Time when arrows point same in current hour: 0{}:0{}".format(int(hour), int(minutes))
+					goingToCollide = True
+					break
+			else:
+				if hour == now.hour and now.minute <= minutes:
+					print "Time when arrows point same in current hour: {}:0{}".format(int(hour), int(minutes))
+					goingToCollide = True
+					break
+		else:
+			if hour/10 == 0:
+				if hour == now.hour and now.minute <= minutes:
+					print "Time when arrows point same in current hour: 0{}:{}".format(int(hour), int(minutes))
+					goingToCollide = True
+					break
+			else:
+				if hour == now.hour and now.minute <= minutes:
+					print "Time when arrows point same in current hour: {}:{}".format(int(hour), int(minutes))
+					goingToCollide = True
+					break
 
 if not goingToCollide:
 	"Sorry - arrows won't collide again this hour!"
