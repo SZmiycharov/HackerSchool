fabfc482df5a86d0d79652c64f897c9f3431f9e9
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-26 10:16:23 +0300

    fix program

--- fast tasks/python/whenClockArrowsCollide.py
+++ fast tasks/python/whenClockArrowsCollide.py
@@ -1,10 +1,26 @@
+import datetime
+now = datetime.datetime.now()
+
 j = 0
 
-for i in range(12, 25):
+timesWhenArrowsCollide = []
+
+for i in range(0, 25):
 	if j == 60:
 		j = 0
 	if j/10 == 0:
-		print "Time when arrows point same: {}:0{}".format(i, j)
+		if i/10 == 0:
+			if i == now.hour:
+				print "Time when arrows point same in current hour: 0{}:0{}".format(i, j)
+		else:
+			if i == now.hour:
+				print "Time when arrows point same in current hour: {}:0{}".format(i, j)
 	else:
-		print "Time when arrows point same: {}:{}".format(i, j)
+		if i/10 == 0:
+			if i == now.hour:
+				print "Time when arrows point same in current hour: 0{}:{}".format(i, j)
+		else:
+			if i == now.hour:
+				print "Time when arrows point same in current hour: {}:{}".format(i, j)
 	j += 5 
+
