feab6709cb9ee1cce6aed5874b0871c0fac66053
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-26 10:35:13 +0300

    minutes <= when arrows collide

--- fast tasks/python/whenClockArrowsCollide.py
+++ fast tasks/python/whenClockArrowsCollide.py
@@ -11,23 +11,23 @@ for i in range(0, 25):
 		j = 0
 	if j/10 == 0:
 		if i/10 == 0:
-			if i == now.hour and now.minute < j:
+			if i == now.hour and now.minute <= j:
 				print "Time when arrows point same in current hour: 0{}:0{}".format(i, j)
 				goingToCollide = True
 				break
 		else:
-			if i == now.hour and now.minute < j:
+			if i == now.hour and now.minute <= j:
 				print "Time when arrows point same in current hour: {}:0{}".format(i, j)
 				goingToCollide = True
 				break
 	else:
 		if i/10 == 0:
-			if i == now.hour and now.minute < j:
+			if i == now.hour and now.minute <= j:
 				print "Time when arrows point same in current hour: 0{}:{}".format(i, j)
 				goingToCollide = True
 				break
 		else:
-			if i == now.hour and now.minute < j:
+			if i == now.hour and now.minute <= j:
 				print "Time when arrows point same in current hour: {}:{}".format(i, j)
 				goingToCollide = True
 				break
