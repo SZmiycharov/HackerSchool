0cd1a733e2bbf716b4c1a0e577bc8f92fdc8e667
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-26 11:11:45 +0300

    fix printing

--- fast tasks/python/whenClockArrowsCollide.py
+++ fast tasks/python/whenClockArrowsCollide.py
@@ -14,25 +14,30 @@ for i in range(0, 24):
 
 	minutes = math.floor(n%60)
 
+
 	if hour < 25:
-		if minutes/10 == 0:
-			if hour/10 == 0:
+		if len(str(int(minutes))) == 1:
+			if len(str(int(hour))) == 1:
+				print "Time when arrows point same in current hour: 0{}:0{}".format(int(hour), int(minutes))
 				if hour == now.hour and now.minute <= minutes:
 					print "Time when arrows point same in current hour: 0{}:0{}".format(int(hour), int(minutes))
 					goingToCollide = True
 					break
 			else:
+				print "Time when arrows point same in current hour: {}:0{}".format(int(hour), int(minutes))
 				if hour == now.hour and now.minute <= minutes:
 					print "Time when arrows point same in current hour: {}:0{}".format(int(hour), int(minutes))
 					goingToCollide = True
 					break
 		else:
-			if hour/10 == 0:
+			if len(str(int(hour))) == 1:
+				print "Time when arrows point same in current hour: 0{}:{}".format(int(hour), int(minutes))
 				if hour == now.hour and now.minute <= minutes:
 					print "Time when arrows point same in current hour: 0{}:{}".format(int(hour), int(minutes))
 					goingToCollide = True
 					break
 			else:
+				print "Time when arrows point same in current hour: {}:{}".format(int(hour), int(minutes))
 				if hour == now.hour and now.minute <= minutes:
 					print "Time when arrows point same in current hour: {}:{}".format(int(hour), int(minutes))
 					goingToCollide = True
