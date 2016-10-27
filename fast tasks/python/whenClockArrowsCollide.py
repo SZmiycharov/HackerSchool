3f6a31ecef29ab15ad014d676481ac437d96ea59
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-27 11:49:34 +0300

    delete more redundant stuff

--- fast tasks/python/whenClockArrowsCollide.py
+++ fast tasks/python/whenClockArrowsCollide.py
@@ -3,8 +3,6 @@ import math
 
 now = datetime.datetime.now()
 
-j = 0
-
 goingToCollide = False
 
 for i in range(0, 24):
@@ -15,8 +13,8 @@ for i in range(0, 24):
 	minutes = math.floor(n%60)
 
 	if hour < 25:
-		print "Time when arrows point same in current hour: %02d:%02d" % (int(hour), int(minutes))
 		if hour == now.hour and now.minute <= minutes:
+			print "Time when arrows point same in current hour: %02d:%02d" % (int(hour), int(minutes))
 			goingToCollide = True
 			break
 
