2febf2861a1ee3b6f0cfb418d7fa6cdddc91c3a8
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-27 13:54:11 +0300

    some small fixesto code

--- fast tasks/python/whenClockArrowsCollide.py
+++ fast tasks/python/whenClockArrowsCollide.py
@@ -5,18 +5,21 @@ now = datetime.datetime.now()
 
 goingToCollide = False
 
-for i in range(0, 24):
+for i in range(0, 23):
 	n = 720 * i / 11
-	n = round(n*100) / 100;
-	hour = math.floor(n/60)
 
+	hour = math.floor(n/60)
 	minutes = math.floor(n%60)
 
-	if hour < 25:
-		if hour == now.hour and now.minute <= minutes:
-			print "Time when arrows point same in current hour: %02d:%02d" % (int(hour), int(minutes))
-			goingToCollide = True
-			break
+	print "Time when arrows point same in current hour: %02d:%02d" % (int(hour), int(minutes))
+	if hour == now.hour and now.minute <= minutes:
+		goingToCollide = True
+		hourToCollide = hour
+		minutesToCollide = minutes
+		break
 
+print "\n"
 if not goingToCollide:
 	print "Sorry - arrows won't collide again this hour!"
+else:
+	print "Time when arrows point same in current hour: %02d:%02d" % (int(hourToCollide), int(minutesToCollide))
