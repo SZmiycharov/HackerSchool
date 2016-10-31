43692ea60d3f0ed019f320de6c015d24e0faf48c
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-31 11:29:42 +0200

    task finished

--- fast tasks/python/supermarket.py
+++ fast tasks/python/supermarket.py
@@ -34,3 +34,42 @@ while True:
          break
 
 
+arrayComb = []
+for i in range (1, n):
+  numFreeProducts = i/k
+  allCombinationsProducts = itertools.combinations(goods, i)
+  for subset in allCombinationsProducts:
+    arrayComb.append(subset)
+
+# arraycomb - po edini4ki, dvoiki, troiki i t.n
+
+currentComb = []
+fullList = []
+okComb = []
+
+totalSum = sum(goods)
+answer = totalSum
+currentAnswer = totalSum
+
+for i in range(2, n):
+  for subset in itertools.combinations(arrayComb, i):
+    for arr in subset:
+      currentComb.append(arr)
+
+    for x in currentComb:
+      fullList += x
+
+    if len(fullList) == n-1 and set(fullList) == set(goods):
+      for slavi in currentComb:
+        freeItems = len(slavi)/k
+        slavi = sorted(slavi)
+        freeitemssum = slavi[0:freeItems]
+        for asd in freeitemssum:
+          currentAnswer -= asd
+        if currentAnswer <= answer:
+          answer = currentAnswer
+    currentAnswer = totalSum     
+    fullList = []
+    currentComb = []
+
+print answer
