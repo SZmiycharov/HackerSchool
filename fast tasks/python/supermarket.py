dd45d90e167951d57a186084cc8dddb4a1d65d11
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-10-31 14:32:42 +0200

    fix the algorithm

--- fast tasks/python/supermarket.py
+++ fast tasks/python/supermarket.py
@@ -1,6 +1,16 @@
 import sys, getopt
 import itertools
 
+def flatten(seq,container=None):
+    if container is None:
+        container = []
+    for s in seq:
+        if hasattr(s,'__iter__'):
+            flatten(s,container)
+        else:
+            container.append(s)
+    return container
+
 while True:
   try:
      nAndk = raw_input()
@@ -35,40 +45,79 @@ while True:
 
 
 shoppingListAllCombinations = []
-for i in range (1, n):
-  numFreeProducts = i/k
-  allCombinationsProducts = itertools.combinations(shoppingList, i)
-  for subset in allCombinationsProducts:
-    shoppingListAllCombinations.append(subset)
+
+for subset in itertools.combinations(shoppingList, k):
+  shoppingListAllCombinations.append(subset)
 
 
-currentCombinationProducts = []
-fullListProducts = []
-okComb = []
-totalSum = sum(shoppingList)
-bestAnswer = totalSum
-currentbestAnswer = totalSum
+curlist = []
+helper = shoppingList
+valid = True
+numFreeProducts = 0
+totalsum = sum(shoppingList)
+answer = sum(shoppingList)
 
-for i in range(2, n):
-  for subset in itertools.combinations(shoppingListAllCombinations, i):
-    for products in subset:
-      currentCombinationProducts.append(products)
 
-    for product in currentCombinationProducts:
-      fullListProducts += product
 
-    if len(fullListProducts) == n-1 and set(fullListProducts) == set(shoppingList):
-      for listProducts in currentCombinationProducts:
-        freeItems = len(listProducts)/k
-        listProducts = sorted(listProducts)
-        freeItemsSum = listProducts[0:freeItems]
-        for freeItem in freeItemsSum:
-          currentbestAnswer -= freeItem
-        if currentbestAnswer <= bestAnswer:
-          bestAnswer = currentbestAnswer
-          
-    currentbestAnswer = totalSum     
-    fullListProducts = []
-    currentCombinationProducts = []
+for subset in itertools.combinations(shoppingListAllCombinations, n/k):
+  helper = shoppingList[:] 
+  for i in subset:
+    for j in i:
+      try:
+        helper.remove(j)
+      except Exception, e:
+        valid = False
 
-print bestAnswer
+  if valid:
+    for groupOfProducts in subset:
+      numFreeProducts = len(groupOfProducts)/k
+      groupOfProducts = sorted(groupOfProducts)
+      totalsum -= sum(groupOfProducts[0:numFreeProducts])
+  if totalsum < answer:
+    answer = totalsum
+
+  totalsum = sum(shoppingList)
+  valid = True
+  helper = shoppingList
+
+print answer
+
+
+# currentCombinationProducts = []
+# fullListProducts = []
+# okComb = []
+# totalSum = sum(shoppingList)
+# bestAnswer = totalSum
+# currentbestAnswer = totalSum
+
+
+# 11 41 4 65 11 90 55 
+
+
+# for i in range(1, n):
+#   for subset in itertools.combinations(shoppingListAllCombinations, i):
+#     for products in subset:
+#       currentCombinationProducts.append(products)
+#     for product in currentCombinationProducts:
+#       fullListProducts += product
+    
+#     if len(fullListProducts) <= n:
+#       for listProducts in currentCombinationProducts:
+#         freeItems = len(listProducts)/k
+#         listProducts = sorted(listProducts)
+#         freeItemsSum = listProducts[0:freeItems]
+
+#         for freeItem in freeItemsSum:
+#           currentbestAnswer -= freeItem
+#         if currentbestAnswer < bestAnswer:
+#           bestAnswer = currentbestAnswer
+#           print subset
+#           print fullListProducts
+#           print "\n"
+
+        
+#     currentbestAnswer = totalSum     
+#     fullListProducts = []
+#     currentCombinationProducts = []
+
+# print bestAnswer
